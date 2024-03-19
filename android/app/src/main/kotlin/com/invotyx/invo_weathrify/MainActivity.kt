a718c379be95bae98a9af2f3a10f8e1f427b2207
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-03-19 16:41:33 +0500

    added main.dart and other required files

---
+++ android/app/src/main/kotlin/com/invotyx/invo_weathrify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.invotyx.zekab_weather
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:zekab_weather/screens/bottom_navigation/bottom_navigation_view.dart';
+import 'helper/theme_helper.dart';
+
+void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  await GetStorage.init();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: Themes.light,
+      themeMode: ThemeMode.system,
+      home: const BottomNavigationView(),
+    );
+  }
+}
